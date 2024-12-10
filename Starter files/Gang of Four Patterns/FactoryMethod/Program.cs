99b12f20c3d51f30cb32cdf0948d053dc3de6da0
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-12-10 19:03:37 -0300

    Factory method pattern implemented.

--- Starter files/Gang of Four Patterns/FactoryMethod/Program.cs
+++ Starter files/Gang of Four Patterns/FactoryMethod/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.Title = "Factory Method";
+﻿using FactoryMethod;
+
+Console.Title = "Factory Method";
+//The key point here is that the client (Program.cs) does not know about any concrete implementations.
+//The client depends on the asbtract classes (theorical speaking, contracts, not concrete implementations).
+
+//In this example, two concrete products were created by our two concrete factories without the client having to know about concrete implementations.
+var factories = new List<DiscountFactory>
+{
+    new CodeDiscountFactory(Guid.NewGuid()),
+    new CountryDiscountFactory("BE")
+};
+
+foreach (var factory in factories)
+{
+    var discountService = factory.CreateDiscountService();
+    Console.WriteLine($"Percentage {discountService.DiscountPercentage} coming from {discountService}");
+}
+
+Console.ReadKey();
