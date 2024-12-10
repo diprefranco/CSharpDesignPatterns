0b8b0575e30f4704e4adae5a9a6c98393e50175b
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-12-10 19:02:27 -0300

    Singleton pattern implemented.

--- Starter files/Gang of Four Patterns/Singleton/Implementation.cs
+++ Starter files/Gang of Four Patterns/Singleton/Implementation.cs
@@ -1,4 +1,40 @@
 ﻿namespace Singleton
 {
-    
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class Logger
+    {
+        protected Logger()
+        {
+        }
+
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+        //private static Logger? _instance;
+
+        /// <summary>
+        /// Instance
+        /// </summary>
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+                //if (_instance == null)
+                //{
+                //    _instance = new Logger();
+                //}
+                //return _instance;
+            }
+        }
+
+        /// <summary>
+        /// SingletonOperation
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
 }
