aab26aa2603cc0f3a8ad1c95e4db017a707313d9
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-12-11 19:47:15 -0300

    Prototype pattern implemented.

--- Starter files/Gang of Four Patterns/Prototype/Implementation.cs
+++ Starter files/Gang of Four Patterns/Prototype/Implementation.cs
@@ -1,4 +1,49 @@
 ﻿namespace Prototype
 {
+    /// <summary>
+    /// Prototype
+    /// </summary>
+    public abstract class Person
+    {
+        public abstract string Name { get; set; }
+        public abstract Person Clone();
+    }
 
+    /// <summary>
+    /// ConcretePrototype
+    /// </summary>
+    public class Manager : Person
+    {
+        public override string Name { get; set; }
+
+        public Manager(string name)
+        {
+            Name = name;
+        }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+
+    /// <summary>
+    /// ConcretePrototype
+    /// </summary>
+    public class Employee : Person
+    {
+        public Manager Manager { get; set; }
+        public override string Name { get; set; }
+
+        public Employee(string name, Manager manager)
+        {
+            Name = name;
+            Manager = manager;
+        }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
 }
--- Starter files/Gang of Four Patterns/Prototype/Program.cs
+++ Starter files/Gang of Four Patterns/Prototype/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.Title = "Prototype";
+﻿using Prototype;
+
+Console.Title = "Prototype";
+
+var manager = new Manager("Cindy");
+var managerClone = (Manager)manager.Clone();
+Console.WriteLine($"Manager was cloned: {managerClone.Name}");
+
+var employee = new Employee("Kevin", manager);
+var employeeClone = (Employee)employee.Clone();
+Console.WriteLine($"Employee was cloned: {employeeClone.Name}, with manager {employeeClone.Manager.Name}");
+
+Console.ReadKey();
